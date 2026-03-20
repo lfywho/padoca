3b2102882f995c05defb838e146d13d65ea5a0dd
Joao Felipe Vicente <161844290+lfywho@users.noreply.github.com>
2026-03-20 12:14:40 -0300

    htaccess

--- config/variaveisgerais.php
+++ config/variaveisgerais.php
@@ -1,11 +1,15 @@
 <?php
 
+    //titulo da página
     $titulopagina = "Padoca - Pão quentinho toda hora";
+
+    //nome da empresa
     $empresaNome = "Padoca & CIA";
+
+    //css principal
     $css = ["main.css"];
+
+    //versão do site
     $vsite = 1;
-
-
-
 
 ?>
